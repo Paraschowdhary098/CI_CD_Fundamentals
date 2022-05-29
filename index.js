78918c5a82f5e71a1ce4c0d5f4d18d1b0bca1f6e
Paras Chowdhary <paraschowdhary098@gmail.com>
2022-05-29 06:54:54 +0000

    Added hello world express app

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.log("Hello world");
 console.log("conetent from dev branch");
+const express = require("express")
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res)=>{
+res.send("Hello world")
+});
+
+app.listen(port, ()=> {
+console.log("Hello world app listening on port 3000");
+})
